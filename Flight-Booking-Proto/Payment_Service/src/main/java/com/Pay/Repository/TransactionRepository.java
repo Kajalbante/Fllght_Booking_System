0dc03b187fff0f6e49ba623ab7b24d19e1aebfad
Kajalbante <kajalbante25@gmail.com>
2025-08-19 11:30:41 +0530

    update

--- Flight-Booking-Proto/Payment_Service/src/main/java/com/Pay/Repository/TransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Pay.Repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.Pay.Entity.Transaction;
-
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-}
--- Flight-Booking-Proto/auth-service/src/main/java/com/auth/exception/DuplicateEmailException.java
+++
@@ -1,7 +0,0 @@
-package com.auth.exception;
-
-public class DuplicateEmailException extends RuntimeException {
-    public DuplicateEmailException(String message) {
-        super(message);
-    }
-}
